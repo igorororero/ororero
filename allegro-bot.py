0312c3b2ac090b13ede813e007a765928d1458ff
igorororero <noskillggg@gmail.com>
2025-04-12 07:21:06 +0200

    Update allegro-bot.py

--- allegro-bot.py
+++ allegro-bot.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Działa! ✅ Bot uruchomiony na Render."
+    return "✅ Bot działa! Flask jest uruchomiony na Render."
 
 if __name__ == "__main__":
-    # Dla Render: nasłuchuj na 0.0.0.0 (nie localhost!) i na porcie 5000
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render ustawia PORT w zmiennych środowiskowych
+    app.run(host="0.0.0.0", port=port)
